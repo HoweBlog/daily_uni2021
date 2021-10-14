8203dad158462db771febffae85d442ada5753ca
Howe <LiaoHowe@qq.com>
2021-10-15 01:14:35 +0800

    轮播图上描述的布局，描述省略未完成

--- CuriosityDaily/pages/index/index.vue
+++ CuriosityDaily/pages/index/index.vue
@@ -6,6 +6,14 @@
 			<swiper-item v-for="(item,index) in swiperImage" :key="index" >
 				<view class="swiper-item">
 					<image :src="item" mode="widthFix"></image>
+					<view class="Copywriter">
+						<view class="desc">
+							<text class="iconfont icon-001danche-2"></text>
+							Howe
+						</view>
+						<view class="title">100861110086111008611100861110086111008611100861110086111008611100861110086111008611100861110086111008611
+						</view>
+					</view>
 				</view>
 			</swiper-item>
 		</swiper>
@@ -38,11 +46,43 @@
 	}
 </script>
 <style lang="scss">	
+	// 引入 css
+	@import url('/iconfonts/iconfont.css');
 	swiper{
 		// 设置 swiper 高度
 		height: calc(100vw * 450/755);
-		image{
-			width: 100vw;
+		swiper-item{
+			position: relative;
+			image{
+				width: 100vw;
+				filter: brightness(0.8);
+			}
+			.Copywriter{
+				position: absolute;
+				left: 0;
+				bottom:40rpx;
+				padding: 0 4%;
+				width: 92%;
+				.desc{
+					background: rgba(0,0,0,0.5);
+					display: inline-block;
+					color: #ffc81f;
+					font-size: 12rpx;
+					padding: 0 2px;
+					line-height: 24rpx;
+					text{
+						display: inline-block;
+						margin-right: 8rpx;
+					}
+				}
+				.title{
+					margin-top: 10rpx;
+					font-size: 46rpx;
+					line-height: 46rpx;
+					color: #fff;
+					overflow: hidden;
+				}
+			}			
 		}
 	}
 </style>
